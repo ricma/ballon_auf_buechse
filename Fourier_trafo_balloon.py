538805885d09904356f0a1964c2b09c8adf287e3
Martin Richter <martin.richter@nottingham.ac.uk>
2018-12-01 22:07:30 +0000

    add playing sound on key press

--- Fourier_trafo_balloon.py
+++ Fourier_trafo_balloon.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.signal
 import pyaudio
+import wave
 import aifc
 
 
@@ -17,13 +18,17 @@ class ToneCallback:
     Register as a callback to play a tone
     """
     def __init__(
-            self, ax,
+            self, ax_time, ax, filename,
             volume=1.0, fs=100000, duration=5, **kwargs):
         """
         Parameters
         ----------
         ax : Subplot
             Subplot with spectrum
+        ax_time : Subplot
+            Subplot with time signal
+        filename : string
+            Recording of drumming the box
         volume : double between [0, 1]
             Volume to play sound with.
         fs : integer
@@ -34,6 +39,8 @@ class ToneCallback:
             If True, then call fig.canvas.mpl_connect on self.
         """
         self.ax = ax
+        self.ax_time = ax_time
+        self.filename = filename
         self.volume = volume
         self.fs = fs
         self.duration = duration
@@ -43,6 +50,7 @@ class ToneCallback:
         if kwargs.get("bind", True):
             fig = self.ax.get_figure()
             fig.canvas.mpl_connect("button_press_event", self)
+            fig.canvas.mpl_connect("key_press_event", self.key)
 
     def __call__(self, event):
         """
@@ -57,6 +65,33 @@ class ToneCallback:
 
         frequency = event.xdata
         self.play_tone(frequency)
+
+    def key(self, event):
+        """
+        Play fine
+        """
+        if event.key != "t":
+            return
+
+        # open the file for reading.
+        print("playing {0}".format(self.filename))
+        with wave.open(self.filename, 'rb') as wf:
+
+            # open stream based on the wave object which has been input.
+            stream = self.p.open(
+                format=self.p.get_format_from_width(
+                    wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True)
+
+            # read data (based on the chunk size)
+            data = wf.readframes(wf.getnframes())
+
+            # writing to the stream is what *actually* plays the sound.
+            stream.write(data)
+            stream.stop_stream()
+            stream.close()
 
     def play_tone(self, freq):
         """
@@ -156,7 +191,7 @@ def analyse(filename, fig=None):
     else:
         ax, ax_f = fig.get_axes()
 
-    t_max_plot = 0.2
+    t_max_plot = 2.2
     slc = slice(None, np.argmin(np.abs(t - t_max_plot)))
     plot_1 = ax.plot(t[slc] * 1000, integers[slc],
             zorder=-10)[0]
@@ -183,16 +218,23 @@ if __name__ == "__main__":
     fig = None
     for filename in [
             # "ballon_auf_Dose_2.wav",
-            "ballon_auf_Dose_3.wav",
-            "ballon_auf_Dose_4.wav"
+            # "ballon_auf_Dose_3.wav",
+            # "ballon_auf_Dose_4.wav",
+            "ballon_auf_Dose_5.wav"
             ]:
 
         fig = analyse("audio/" + filename, fig=fig)
 
-    filename = "pictures/ballon_auf_Dose.png"
-    plt.savefig(filename, transparent=True)
-    print("[[file:{0}]]".format(filename))
+    filename_png = "pictures/ballon_auf_Dose.png"
+    plt.savefig(filename_png, transparent=True)
+    print("[[file:{0}]]".format(filename_png))
 
+    ax_time = fig.get_axes()[0]
     ax_frequency = fig.get_axes()[1]
-    with ToneCallback(ax_frequency) as tc:
+
+    print("""
+Press 't' for play the tone,
+click on the maxima right to play sine tone""")
+    with ToneCallback(ax_time, ax_frequency, "audio/" + filename,
+                      duration=10, volume=0.1) as tc:
         plt.show()
